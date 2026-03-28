31285c8ec65237fc7d5c4c38fa633a58cd8a18b1
Færkeren <164513459+Faerkeren@users.noreply.github.com>
2026-03-28 20:42:44 -0200

    Some fixes

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,6 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-
 from app.database import DATABASE_URL
 from app.models import Base
 
--- alembic/versions/5b64dd02b186_initial_schema.py
+++ alembic/versions/5b64dd02b186_initial_schema.py
@@ -6,7 +6,7 @@ Create Date: 2026-03-28 18:56:52.223372
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 import sqlalchemy as sa
 
@@ -14,9 +14,9 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "5b64dd02b186"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
